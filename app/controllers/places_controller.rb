88b56442abfb18a73f720c63da890601e64af25b
Marian Serna <marianhalliday@gmail.com>
2017-11-02 22:01:38 -0400

    Setting up scopes in the places controller + tests

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,12 @@
 class PlacesController < ApplicationController
+
   def index
-    places = Place.all
+    places = Place.near([params[:lat], params[:lon]])
+
+    places = places.by_activity_type(params[:activity_type]) if params[:activity_type]
+
+    places = places.by_category(params[:category]) if params[:category]
+
     render json: places
   end
 end
--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -2,14 +2,98 @@ require 'rails_helper'
 
 RSpec.describe PlacesController, type: :controller do
   describe 'index' do
-    it 'returns places as json' do
+    it 'doesnt find place by lat and lon' do
       create(:place)
-      
-      get :index
+
+      get :index, params: {
+        lat: '6.244203',
+        lon: '-75.581212'
+      }
+
+      expect(response.status).to eq(200)
+
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body.length).to eq(0)
+    end
+
+    it 'finds place by lat and lon' do
+      create(:place)
+
+      get :index, params: {
+        lat: '43.653226',
+        lon: '-79.383184'
+      }
+
       expect(response.status).to eq(200)
 
       parsed_body = JSON.parse(response.body)
       expect(parsed_body.length).to eq(1)
     end
+
+    it 'finds places by activity_type' do
+      place = create(:place)
+
+      get :index, params: {
+        lat: '43.653226',
+        lon: '-79.383184',
+        activity_type: 'traditional'
+      }
+
+      expect(response.status).to eq(200)
+
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body.first).to eq({
+        "activity_type" => "traditional",
+        "address" => "93 Front St E",
+        "category" => "food",
+        "city" => {
+          "id"=>place.city_id,
+          "name"=>"Toronto",
+          "country"=>"CA",
+          "province"=>"ON",
+          "lat"=>"43.65323",
+          "lon"=>"-79.38318"
+        },
+        "id" => place.id,
+        "lat" => "43.64266",
+        "lon" => "-79.36966",
+        "name" => "St. Lawrence Market",
+        "postal_code" => "M5E 1C3",
+      })
+    end
+
+    it 'finds places by category' do
+      place = create(:place)
+
+      get :index, params: {
+        lat: '43.653226',
+        lon: '-79.383184',
+        category: 'food'
+      }
+
+      expect(response.status).to eq(200)
+
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body.first).to eq({
+        "activity_type" => "traditional",
+        "address" => "93 Front St E",
+        "category" => "food",
+        "city" => {
+          "id"=>place.city_id,
+          "name"=>"Toronto",
+          "country"=>"CA",
+          "province"=>"ON",
+          "lat"=>"43.65323",
+          "lon"=>"-79.38318"
+        },
+        "id" => place.id,
+        "lat" => "43.64266",
+        "lon" => "-79.36966",
+        "name" => "St. Lawrence Market",
+        "postal_code" => "M5E 1C3",
+      })
+    end
   end
+
+
 end
