78fc2f979cc648daa0fe11a4e1ac05895e386255
Marian Serna <marianhalliday@gmail.com>
2017-11-04 12:53:51 -0400

    Creating LoadPlaces class + test

---
+++ app/models/load_places.rb
@@ -0,0 +1,13 @@
+class LoadPlaces
+  attr_accessor :lat, :lon, :client
+
+  def initialize(lat, lon)
+    @lat = lat
+    @lon = lon
+    @client = GooglePlaces::Client.new(ENV.fetch("GOOGLE_PLACES_API_KEY"))
+  end
+
+  def load
+    @client.spots(lat, lon, detail: true)
+  end
+end
---
+++ spec/models/load_places_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe LoadPlaces do
+  describe 'load' do
+    it 'returns places from google' do
+      loader = LoadPlaces.new(43.618870, -80.144189)
+      google_places = loader.load
+
+      expect(google_places.present?).to be(true)
+      expect(google_places.first).to be_a(GooglePlaces::Spot)
+    end
+  end
+
+end
