1a88f4a45218fc29d0426c92ac1bd154d6b4eb03
Marian Serna <marianhalliday@gmail.com>
2017-11-19 16:02:40 -0500

    trips index route and action

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,10 +1,30 @@
 class TripsController < ApplicationController
 
+  def index
+  # SELECT * FROM trips
+  # WHERE user_id = 9
+  # OR id IN (
+  # 	SELECT trip_id FROM buddies
+  # 	WHERE user_id = 9
+  # )
+
+    trip_ids = Buddy.where(user_id: current_user["id"]).pluck(:trip_id)
+    trips = Trip.
+      where("user_id = ? OR id IN (?)", current_user["id"], trip_ids)
+
+    render json: trips
+  end
+
   def create
     trip = Trip.new(trip_params)
     trip.user_id = current_user["id"]
-    # raise trip.inspect
     trip.save
+
+    render json: trip
+  end
+
+  def show
+    trip = Trip.find(params[:id])
 
     render json: trip
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :places, only: [:index, :show]
-  resources :trips, only: [:create]
+  resources :trips, only: [:index, :create, :show]
 end
