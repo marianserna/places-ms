3960c4316b5419477b68506137fc37cc7e4d8dae
Marian Serna <marianhalliday@gmail.com>
2017-11-27 20:59:22 -0500

    Loading places before searching for near ones (Shows places when user searches)

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,8 @@
 class PlacesController < ApplicationController
 
   def index
+    Place.load_and_save(params[:lat], params[:lon])
+    
     places = Place.near([params[:lat], params[:lon]])
     places = places.by_activity_type(params[:activity_type]) if params[:activity_type]
     places = places.by_category(params[:category]) if params[:category]
