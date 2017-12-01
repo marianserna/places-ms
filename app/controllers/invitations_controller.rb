9e4c97361addf6efb405d44d21205463570b57b9
Marian Serna <marianhalliday@gmail.com>
2017-12-01 07:14:20 -0500

    Allow any buddy to send an invitation

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,7 +1,7 @@
 class InvitationsController < ApplicationController
 
   def create
-    trip = Trip.where(user_id: current_user["id"]).find(params[:trip_id])
+    trip = Trip.find(params[:trip_id])
     email = params[:email]
 
     InvitationMailer.buddy_invite(email, current_user, trip).deliver_now
