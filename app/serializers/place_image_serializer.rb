23cf77e852766296284a15267ab953a37bf9f2a3
Marian Serna <marianhalliday@gmail.com>
2017-12-12 08:10:03 -0500

    Removing detail size from place images to make app faster

--- app/serializers/place_image_serializer.rb
+++ app/serializers/place_image_serializer.rb
@@ -1,5 +1,5 @@
 class PlaceImageSerializer < ActiveModel::Serializer
-  attributes :id, :card_image, :detail_image
+  attributes :id, :card_image
 
   belongs_to :place
 
@@ -9,18 +9,13 @@ class PlaceImageSerializer < ActiveModel::Serializer
     local_img_url(url)
   end
 
-  def detail_image
-    url = object.image.detail.url
-    local_img_url(url)
-  end
-
   private
 
   def local_img_url(url)
     if url.start_with?('http')
       url
     else
-      "http://localhost:5000/#{url}"      
+      "http://localhost:5000/#{url}"
     end
   end
 end
--- app/uploaders/place_image_uploader.rb
+++ app/uploaders/place_image_uploader.rb
@@ -33,10 +33,6 @@ class PlaceImageUploader < CarrierWave::Uploader::Base
     process resize_to_fill: [512, 384]
   end
 
-  version :detail do
-    process resize_to_fill: [1024, 768]
-  end
-
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
